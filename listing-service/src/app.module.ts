de9cc90d325857a88ee3679cf1d5dd27cbcbc7ca
skyish242 <atap2749@gmail.com>
2023-10-30 02:56:27 +0800

    listing folder rechanged

--- listing-service/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ListingModule } from './listing/listing.module';
-
-@Module({
-  imports: [ListingModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
